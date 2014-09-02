ca898a6a9ba1f75e4785069ae3290583964a250f
Hypnoatic <hypnoatic@aol.com>
2014-09-02 15:51:23 -0400

    timestamp

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 $(document).ready(function (){
 	var test = 17;
 	var d = new Date();
-	document.getElementById('date').appendChild("Posted on:' + d.getDate() + ' ' +(d.getMonth + 1) + ' ' + d.getFullYear() + 'at ' + d.getHours() + ':' + d.getMinutes()");
+	var newText = "Posted on:" d.getDate() + " " + (d.getMonth + 1) + " " + d.getFullYear() + "at " + d.getHours() ":" + d.getMinutes() ";
+	document.getElementById('date').appendChild(newText);
  
 
 })
