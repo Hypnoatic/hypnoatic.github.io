75281a206516b96240191262781d72fad7cc8a2d
Hypnoatic <hypnoatic@aol.com>
2014-09-02 15:20:20 -0400

    timestamp

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-var name = "#footer";
-var menuYloc = null;
-var $ = 
- 
-$(document).ready(function(){
-  menuYloc = parseInt($(name).css("top").substring(0,$(name).css("top").indexOf("px")))
-    $(window).scroll(function () { 
-        var offset = menuYloc+$(document).scrollTop()+"px";
-        $(name).animate({top:offset},{duration:500,queue:false});
-  });
+$(document).ready(function (){
 	var d = new Date();
-	var paragraph = document.createElement('p');
-	document.getElementById('date').appendChild('Posted on :' + d.getDate() + ' ' + (d.getMonth() + 1) + ' ' + d.getFullYear() + 'at ' + d.getHours() + ':' + d.getMinutes()');
+	document.getElementById('date').appendChild('Posted on:' + d.getDate() + ' ' +(d.getMonth + 1) + ' ' + d.getFullYear() + 'at ' + d.getHours() + ':' + d.getMinutes()';)
+ 
 
-
-});
+})
