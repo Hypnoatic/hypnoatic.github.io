191ea85b50705d7cc3b0fb8bd5e4e1992f1eff38
Hypnoatic <hypnoatic@aol.com>
2014-09-02 15:00:41 -0400

    working timestamp

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ var name = "#footer";
 var menuYloc = null;
  
 $(document).ready(function(){
-    menuYloc = parseInt($(name).css("top").substring(0,$(name).css("top").indexOf("px")))
+  menuYloc = parseInt($(name).css("top").substring(0,$(name).css("top").indexOf("px")))
     $(window).scroll(function () { 
         var offset = menuYloc+$(document).scrollTop()+"px";
         $(name).animate({top:offset},{duration:500,queue:false});
-    });
+  });
+	var d = new Date();
+	var paragraph = document.createElement('p');
+	docutment.getElementById('date').appendChild('Posted on :' + d.getDate() ' ' + (d.getMonth() + 1) + ' ' + d.getFullYear() + 'at ' + d.getHours() + ':' + d.getMinutes());
+
+
 });
