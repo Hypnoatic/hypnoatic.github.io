640cca37c834fa472a67448d783dadef992c3bd1
Hypnoatic <hypnoatic@aol.com>
2014-09-02 15:54:46 -0400

    no idea

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function (){
 	var test = 17;
 	var d = new Date();
-	var newText = "Posted on:" d.getDate() + " " + (d.getMonth + 1) + " " + d.getFullYear() + "at " + d.getHours() ":" + d.getMinutes() ";
+	var text = "Posted on:" d.getDate() + " " + (d.getMonth + 1) + " " + d.getFullYear() + "at " + d.getHours() ":" + d.getMinutes() ";
 	document.getElementById('date').appendChild(newText);
  
 
