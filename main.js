da14bee25e985777169b4a5778042ceb3833a3fb
Hypnoatic <hypnoatic@aol.com>
2014-09-09 14:59:17 -0400

    te

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ $(document).ready(function (){
 	text += checkAM(d.getHours())
 	$('p.date').text(text);
 	var height = $('.post p').height();
-	$('.post').css("height", height + 90);
+	$('.post').css("height", height + 100);
 
 	function checkHour(hour) {
 		if (hour - 12 <= 0 ) return hour;
